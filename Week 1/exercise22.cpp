42cbe4eb22ad2579280dd43f0a26e2ba30004227
Hafizh Husen <hfzh21@outlook.com>
2021-09-14 19:25:26 +0700

    add ex22

---
+++ Week 1/exercise22.cpp
@@ -0,0 +1,28 @@
+// Classes and Objects
+
+#include <iostream>
+using namespace std;
+
+class Book {
+    public:
+        string title, author;
+        int pages;
+};
+
+int main(){
+
+    Book book1;
+    book1.title = "Harry Potter";
+    book1.author = "JK Rowling";
+    book1.pages = 500;
+
+    Book book2;
+    book2.title = "Lord of the Rings";
+    book2.author = "Tolkien";
+    book2.pages = 700;
+
+    
+    cout << book2.author;
+
+    return 0;
+}
