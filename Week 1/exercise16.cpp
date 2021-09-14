6ee359a0b181a7bcf88ca7e1cd62a550c3870dd6
Hafizh Husen <hfzh21@outlook.com>
2021-09-14 17:35:23 +0700

    add ex16

---
+++ Week 1/exercise16.cpp
@@ -0,0 +1,23 @@
+// While Loop
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int index = 6;
+    /* While Loops
+    while (index <= 5)
+    {
+        cout << index << endl;
+        index++;
+    }*/
+
+    // Do While Loops
+    do{
+        cout << index << endl;
+        index++;
+    }while(index <= 5);
+    
+    return 0;
+}
